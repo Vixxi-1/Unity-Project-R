ea3bb45add463e29d83704f83b695bc36ce8553a
Tran_Vivian <tranv032@hsd.k12.or.us>
2018-10-19 13:22:38 -0700

    add rigidbody, add force

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    private Rigidbody m_rb;
 
 	// Use this for initialization
 	void Start () {
-		
+        m_rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void FixedUpdate()
+    {
+        /* 
+         * get user input to add a left-right 
+         * force to our player object 
+         */
+
+         // Get user input
+        float movement = Input.GetAxis("Horizontal");
+        // Add force
+        m_rb.AddForce(new Vector3(movement, 0.0F, 0.0F));
+    }
 }
